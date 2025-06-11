fa006288854c031a6549b4e4fb4f2ccf803f7d8c
wuys <quynn@air.local>
2025-06-11 18:00:17 +0700

    quynn: update

--- sb_git.ts
+++ sb_git.ts
@@ -6,10 +6,13 @@ interface IGitInitPayload {
   email: string;
 }
 
+// TODO: Add support SSH clone?
 export const gitClone = async () => {
+  // TODO: Handle HTTP and SSH
   const url = await editor.prompt(`Project URL:`);
   if (!url) return;
 
+  // TODO: Allow input SSH Key if use SSH
   const token = await editor.prompt(
     `Access token (For GitLab: username:access_token):`,
   );
@@ -21,16 +24,24 @@ export const gitClone = async () => {
   const email = await editor.prompt(`Your email:`);
   if (!email) return;
 
-  const parts = url.split("/");
-  parts[2] = `${token}@${parts[2]}`;
+  // TODO: Add confirmation box
+  const confirm = await editor.prompt(
+    `Your content in git repo will overide current space, continue?(Yes/No)`,
+  );
+  if (confirm?.toLowerCase() == "yes" || confirm?.toLowerCase() == "y") {
+    const parts = url.split("/");
+    parts[2] = `${token}@${parts[2]}`;
 
-  const payload: IGitInitPayload = {
-    url: parts.join("/") + ".git",
-    name,
-    email,
-  };
-
-  await initRepo(payload);
+    const payload: IGitInitPayload = {
+      url: parts.join("/"), // No need .git at the end if use HTTP
+      name,
+      email,
+    };
+    await initRepo(payload);
+  } else {
+    editor.flashNotification(`User cancelled.`);
+    return;
+  }
 };
 
 export const sync = async () => {
@@ -65,6 +76,7 @@ export const replaceToken = async () => {
   }
 
   // Update new origin
+  console.log("Updating git remote");
   await shell.run("git", ["remote", "set-url", "origin", newUrl]);
   await editor.flashNotification(`Replaced token successfully!`);
 };
@@ -72,11 +84,12 @@ export const replaceToken = async () => {
 export const changeGitRepo = async () => {
   console.log(`Deleting old .git folder`);
   await shell.run("rm", ["-rf", ".git"]);
-  console.log(`Trigger gitClone`);
+  console.log(`Trigger git clone`);
   await gitClone();
 };
 
 export const scheduleCommit = async () => {
+  // TODO: Handle CONFIG.md
   const currentMin = new Date().getMinutes();
   if (currentMin % 5 === 0) {
     await sync();
@@ -87,15 +100,24 @@ const initRepo = async (payload: IGitInitPayload) => {
   await editor.flashNotification(
     "Cloning your git repo, it might take some time.",
   );
-  await shell.run("git", ["clone", payload.url, "_sb_git"]);
-
+  try {
+    await shell.run("git", ["clone", payload.url, "_sb_git"]);
+  } catch {
+    console.error("Failed to clone repository, please check your details");
+    await editor.flashNotification(
+      "Failed to clone repository, please check your details",
+    );
+    return;
+  }
   // Create .gitignore file
+  // TODO: Check if .gitignore exist, then skip
   console.log(`Creating .gitignore file`);
   await shell.run("echo", [".silverbullet.db*", ">", ".gitignore"]);
   await shell.run("echo", ["_plug/", ">>", ".gitignore"]);
   await shell.run("echo", ["Library/", ">>", ".gitignore"]);
 
   // Move content and .git folder from _sb_git
+  console.log("Moving repo content to space");
   await shell.run("bash", ["-c", "mv -f _sb_git/{.,}* . 2> /dev/null; true"]);
   await shell.run("rm", ["-rf", "_sb_git"]);
   await shell.run("git", ["config", "user.name", payload.name]);
